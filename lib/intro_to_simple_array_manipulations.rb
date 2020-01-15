cb3cb27f682c9079dda3a6e6a5bee2478aba43a7
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-15 00:35:29 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,13 @@
+artists = ["Hozier", "Ariana Grande", "Usher"]
+next_artist = "Beyonce"
+artists.push(next_artist)
+
+artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists.pop
+
+artists.pop(2)
+
+.shift [0]
+
+.shift [0,1]
+
